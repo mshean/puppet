ab3231669982bc5a79f56e940ce2a1d62625bfca
Matt Shean <mattshean@gmail.com>
2014-04-19 23:07:10 +0000

    added tests for PUP-2073

--- spec/unit/util/diff_spec.rb
+++ spec/unit/util/diff_spec.rb
@@ -2,6 +2,7 @@
 require 'spec_helper'
 require 'puppet/util/diff'
 require 'puppet/util/execution'
+require 'tempfile'
 
 describe Puppet::Util::Diff do
   describe ".diff" do
@@ -26,6 +27,53 @@ describe Puppet::Util::Diff do
 
       Puppet::Util::Execution.expects(:execute).never
       subject.diff('a', 'b').should == ''
+    end
+
+    it "should correctly diff files without arguments" do
+      Puppet[:diff] = 'diff'
+      tempfileA = Tempfile.new("puppet-diffingA")
+      tempfileB = Tempfile.new("puppet-diffingB")
+      tempfileA.open
+      tempfileB.open
+      tempfileA.print "hello\n"
+      tempfileB.print "world\n"
+      tempfileA.close
+      tempfileB.close
+      subject.diff(tempfileA.path, tempfileB.path).should include("@@ -1 +1 @@\n-hello\n\+world\n")
+      tempfileA.delete
+      tempfileB.delete
+    end
+
+    it "should correctly diff files with an argument" do
+      Puppet[:diff] = 'diff'
+      Puppet[:diff_args] = '-u'
+      tempfileA = Tempfile.new("puppet-diffingA")
+      tempfileB = Tempfile.new("puppet-diffingB")
+      tempfileA.open
+      tempfileB.open
+      tempfileA.print "hello\n"
+      tempfileB.print "world\n"
+      tempfileA.close
+      tempfileB.close
+      subject.diff(tempfileA.path, tempfileB.path).should include("@@ -1 +1 @@\n-hello\n\+world\n")
+      tempfileA.delete
+      tempfileB.delete
+    end
+
+    it "should correctly diff files with multiple arguments" do
+      Puppet[:diff] = 'diff'
+      Puppet[:diff_args] = '-u --strip-trailing-cr'
+      tempfileA = Tempfile.new("puppet-diffingA")
+      tempfileB = Tempfile.new("puppet-diffingB")
+      tempfileA.open
+      tempfileB.open
+      tempfileA.print "hello\n"
+      tempfileB.print "world\n"
+      tempfileA.close
+      tempfileB.close
+      subject.diff(tempfileA.path, tempfileB.path).should include("@@ -1 +1 @@\n-hello\n\+world\n")
+      tempfileA.delete
+      tempfileB.delete
     end
   end
 end
